e51d2d399ffe1e84ce37d375551c91e53d3979ba
Seanforfun <32276204+Seanforfun@users.noreply.github.com>
2018-02-28 21:31:39 -0500

    [Function add]
    1.Create model for orderModel.

--- src/main/java/ca/mcmaster/erp/invoice/order/model/OrderModel.java
+++ src/main/java/ca/mcmaster/erp/invoice/order/model/OrderModel.java
@@ -1,11 +1,97 @@
 package ca.mcmaster.erp.invoice.order.model;
+
+import ca.mcmaster.erp.auth.emp.model.EmpModel;
+
 /**
  * @author SeanForFun E-mail:xiaob6@mcmaster.ca
  * @version Feb 24, 2018 10:03:16 PM
  */
 public class OrderModel {
 	private Long uuid;
-	
+	private String orderNum;
+	private EmpModel creator;
+	private Long createTime;
+	private EmpModel checker;
+	private Long checkTime;
+	private EmpModel completer;
+	private Long endTime;
+	private Integer orderType;
+	private Integer type;
+	private Integer totalNum;
+	private Double totalPrice;
+	private EmpModel supplierUuid;
+	public String getOrderNum() {
+		return orderNum;
+	}
+	public void setOrderNum(String orderNum) {
+		this.orderNum = orderNum;
+	}
+	public EmpModel getCreator() {
+		return creator;
+	}
+	public void setCreator(EmpModel creator) {
+		this.creator = creator;
+	}
+	public Long getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(Long createTime) {
+		this.createTime = createTime;
+	}
+	public EmpModel getChecker() {
+		return checker;
+	}
+	public void setChecker(EmpModel checker) {
+		this.checker = checker;
+	}
+	public Long getCheckTime() {
+		return checkTime;
+	}
+	public void setCheckTime(Long checkTime) {
+		this.checkTime = checkTime;
+	}
+	public EmpModel getCompleter() {
+		return completer;
+	}
+	public void setCompleter(EmpModel completer) {
+		this.completer = completer;
+	}
+	public Long getEndTime() {
+		return endTime;
+	}
+	public void setEndTime(Long endTime) {
+		this.endTime = endTime;
+	}
+	public Integer getOrderType() {
+		return orderType;
+	}
+	public void setOrderType(Integer orderType) {
+		this.orderType = orderType;
+	}
+	public Integer getType() {
+		return type;
+	}
+	public void setType(Integer type) {
+		this.type = type;
+	}
+	public Integer getTotalNum() {
+		return totalNum;
+	}
+	public void setTotalNum(Integer totalNum) {
+		this.totalNum = totalNum;
+	}
+	public Double getTotalPrice() {
+		return totalPrice;
+	}
+	public void setTotalPrice(Double totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+	public EmpModel getSupplierUuid() {
+		return supplierUuid;
+	}
+	public void setSupplierUuid(EmpModel supplierUuid) {
+		this.supplierUuid = supplierUuid;
+	}
 	public Long getUuid() {
 		return uuid;
 	}
